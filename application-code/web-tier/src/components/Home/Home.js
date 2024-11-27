61060acf3e11b9349f054d0af8b6758eb7529ddb
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-11-27 19:33:43 +0530

    change stype height to auto in architecture image

--- application-code/web-tier/src/components/Home/Home.js
+++ application-code/web-tier/src/components/Home/Home.js
@@ -7,7 +7,7 @@
         return (
             <div>
             <h1 style={{color:"white"}}>AWS 3-TIER WEB APP DEMO</h1>
-            <img src={architecture} alt="3T Web App Architecture" style={{height:400,width:825}} />
+            <img src={architecture} alt="3T Web App Architecture" style={{height:'auto',width:950}} />
           </div>
         );
       }
